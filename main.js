7a8bf8f3aadbfa06e6996f95e4ea2193a9be5266
Kevin Le <letuanhaidang369@gmail.com>
2024-01-20 13:00:13 -0600

    Fixing errors appear in conditions that not allow typing two operators respectively

--- main.js
+++ main.js
@@ -1,24 +1,31 @@
 document.addEventListener('DOMContentLoaded', function () {
     const screen = document.querySelector('#screen');
-    const allButtons = document.querySelectorAll('.basic');
+    const numberButtons = document.querySelectorAll('.basic');
     const clearButton = document.querySelector('#clearButton');
     const deleteButton = document.querySelector('#deleteButton');
+    const operatorButtons = document.querySelectorAll('.advance');
 
-    allButtons.forEach(button => button.addEventListener('click', () => {
-        let lastChar = screen.textContent.slice(-1);
+    numberButtons.forEach(button => button.addEventListener('click', () => {
+        if (screen.textContent === '0') {
+            screen.textContent = '';        
+        } 
+        screen.textContent += button.innerHTML;
+    }));
+
+    operatorButtons.forEach(button => button.addEventListener('click', () => {
 
         if (screen.textContent === '0') {
-            screen.textContent = '';
-        }
-
-        else if (lastChar === '.' && lastChar === '+' && lastChar === '-' && lastChar === 'x' && lastChar === '÷') {
-            screen.textContent = screen.textContent;
-        }
-
-        else {
+            screen.textContent = '0';        
+        } else {
             screen.textContent += button.innerHTML;
         }
-        
+
+        let lastChar = screen.textContent.slice(-2, -1);
+
+        if (lastChar === '.' || lastChar === '+' || lastChar === '-' || lastChar === 'x' || lastChar === '÷') {
+            screen.textContent = screen.textContent.slice(0, -1);
+        }
+
     }));
 
     clearButton.addEventListener('click', clearScreen);
@@ -29,7 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
     deleteButton.addEventListener('click', deleteLastCharInScreen);
     function deleteLastCharInScreen () {
         if (screen.textContent !== '0') {
-        screen.textContent = screen.textContent.slice(0, -1);
+            screen.textContent = screen.textContent.slice(0, -1);
         };
         if (screen.textContent === '') {
             screen.textContent = '0'
