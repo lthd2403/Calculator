c7559fdf788e9f1770174605677632cd1df2a073
Kevin Le <letuanhaidang369@gmail.com>
2024-01-19 21:33:51 -0600

    Add funnctions to clear and to delete one charactor in screen

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
     const screen = document.querySelector('#screen');
-    const allButtons = document.querySelectorAll('button');
-    console.log(allButtons);
-
+    const allButtons = document.querySelectorAll('.basic');
+    const clearButton = document.querySelector('#clearButton');
+    const deleteButton = document.querySelector('#deleteButton');
 
     allButtons.forEach(button => button.addEventListener('click', () => {
         if (screen.textContent === '0') {
-            screen.textContent = "";
+            screen.textContent = '';
         };
-        
-        screen.textContent += button.textContent;
+        screen.textContent += button.innerHTML;
     }));
+
+    clearButton.addEventListener('click', clearScreen);
+    function clearScreen() {
+        screen.textContent = '0';
+    };
+
+    deleteButton.addEventListener('click', deleteOneElementInScreen);
+    function deleteOneElementInScreen () {
+        screen.textContent = screen.textContent.slice(0, -1);
+    };
 });
+
